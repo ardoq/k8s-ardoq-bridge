6e457ce7b92c15da7d7e26a19503813388c8cea1
Ian Muge <ian.muge@ardoq.com>
2021-12-30 14:51:53 +0100

    parse all images in the resource

--- controllers/bridgecontroller.go
+++ controllers/bridgecontroller.go
@@ -3,13 +3,23 @@ package controllers
 import (
 	"context"
 	v1 "k8s.io/api/apps/v1"
+	v12 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog"
+	"strings"
 )
 
 type BridgeController struct {
 	KubeClient *kubernetes.Clientset
+}
+
+func getContainerImages(containers []v12.Container) string {
+	values := make([]string, 0, len(containers))
+	for _, v := range containers {
+		values = append(values, v.Image)
+	}
+	return strings.Join(values, ", ")
 }
 
 func (b *BridgeController) OnDeploymentEvent(event watch.Event, res *v1.Deployment) {
@@ -26,7 +36,7 @@ func (b *BridgeController) OnDeploymentEvent(event watch.Event, res *v1.Deployme
 		ID:        "",
 		Namespace: res.Namespace,
 		Replicas:  int64(res.Status.Replicas),
-		Image:     res.Spec.Template.Spec.Containers[0].Image,
+		Image:     getContainerImages(res.Spec.Template.Spec.Containers),
 	}
 	switch event.Type {
 	case watch.Added, watch.Modified:
@@ -51,7 +61,7 @@ func (b *BridgeController) OnStatefulsetEvent(event watch.Event, res *v1.Statefu
 		ID:        "",
 		Namespace: res.Namespace,
 		Replicas:  int64(res.Status.Replicas),
-		Image:     res.Spec.Template.Spec.Containers[0].Image,
+		Image:     getContainerImages(res.Spec.Template.Spec.Containers),
 	}
 	switch event.Type {
 	case watch.Added, watch.Modified:
