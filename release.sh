e61bec762e6cfc1cf3e061a8b19d8409e3bc499d
Ian Muge <ian.muge@ardoq.com>
2022-01-11 14:42:58 +0100

    semver releasing

--- release.sh
+++ release.sh
@@ -5,8 +5,50 @@ if ! git diff --quiet; then
     echo "You have uncommitted changes, please commit them before running release."
     exit 1
 fi
+
+RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
+
+step="$1"
+if [ -z "$1" ]
+then
+  step="patch"
+fi
+
+base="$2"
+if [ -z "$2" ]
+then
+  base=$(git tag 2>/dev/null| tail -n 1)
+  if [ -z "$base" ]
+  then
+    base=0.0.0
+  fi
+fi
+
+# shellcheck disable=SC2001
+MAJOR=$(echo $base | sed -e "s#$RE#\1#")
+# shellcheck disable=SC2001
+MINOR=$(echo $base | sed -e "s#$RE#\2#")
+# shellcheck disable=SC2001
+PATCH=$(echo $base | sed -e "s#$RE#\3#")
+
+case "$step" in
+  patch)
+      PATCH=$(( PATCH + 1 ))
+  ;;
+  minor)
+      MINOR=$(( MINOR + 1 ))
+      PATCH=0
+  ;;
+  major)
+    MAJOR=$(( MAJOR + 1 ))
+    MINOR=0
+    PATCH=0
+  ;;
+
+esac
+
 image_version_file="image_version.txt"
-new_version=$(( $(cat ${image_version_file}) + 1 ))
+new_version="$MAJOR.$MINOR.$PATCH"
 
 echo "$new_version" > ${image_version_file}
 
