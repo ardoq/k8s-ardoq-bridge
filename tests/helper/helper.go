3b48197bbd9422f45965eae3133970e690bcfd06
Ian Muge <ian.muge@ardoq.com>
2022-01-11 17:51:20 +0100

    better randomization of test data

--- tests/helper/helper.go
+++ tests/helper/helper.go
@@ -12,12 +12,14 @@ func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min)
 }
 
-func RandomString(len int) string {
-	byts := make([]byte, len)
-	for i := 0; i < len; i++ {
-		byts[i] = byte(RandomInt(65, 90))
+func RandomString(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
 	}
-	return string(byts)
+	return string(b)
 }
 func Contains(s []string, str string) bool {
 	for _, v := range s {
