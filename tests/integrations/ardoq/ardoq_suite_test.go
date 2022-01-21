dfa981ca26e358b73d52b5010d6d787e6c5413ab
Ian Muge <ian.muge@ardoq.com>
2022-01-21 23:03:30 +0100

    again, fix race condition if ran in parallel

--- tests/integrations/ardoq/ardoq_suite_test.go
+++ tests/integrations/ardoq/ardoq_suite_test.go
@@ -2,6 +2,7 @@ package ardoq_test
 
 import (
 	"K8SArdoqBridge/app/controllers"
+	"K8SArdoqBridge/app/tests/helper"
 	"k8s.io/klog/v2"
 	"os"
 	"testing"
@@ -16,7 +17,13 @@ func TestArdoqController(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-
+	klog.Info("Initializing")
+	err := os.Setenv("ARDOQ_CLUSTER", helper.RandomString(5)+"-"+os.Getenv("ARDOQ_CLUSTER"))
+	if err != nil {
+		klog.Error(err)
+	}
+	klog.Infof("Creating cluster: %q", os.Getenv("ARDOQ_CLUSTER"))
+	klog.Info("Initializing Complete")
 })
 
 var _ = AfterSuite(func() {
