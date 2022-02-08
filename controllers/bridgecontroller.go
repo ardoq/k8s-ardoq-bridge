e93e539aa27c718ca410e7cc8cb466451067d2ca
Ian Muge <ian.muge@ardoq.com>
2022-02-08 18:10:03 +0100

    cleanup we dont need to watch namespaces if we are already checking namespace labels

--- controllers/bridgecontroller.go
+++ controllers/bridgecontroller.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	v1 "k8s.io/api/apps/v1"
 	v12 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
@@ -79,33 +78,7 @@ func (b *BridgeController) OnApplicationResourceEvent(event watch.Event, generic
 		break
 	}
 }
-func (b *BridgeController) OnNamespaceEvent(event watch.Event, res *v12.Namespace) {
-	resourceType := "Namespace"
-	if res.Name == "" {
-		klog.Errorf("Unable to retrieve %s from incoming event", resourceType)
-		return
-	}
 
-	deploys, err := ClientSet.AppsV1().Deployments(res.Name).List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		klog.Errorf("failed to get List deployments: %w", err)
-	}
-	for _, d := range deploys.Items {
-		if d.GetLabels()["sync-to-ardoq"] != "disabled" {
-			b.OnApplicationResourceEvent(event, d)
-		}
-
-	}
-	sts, err := ClientSet.AppsV1().StatefulSets(res.Name).List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		klog.Errorf("failed to get List deployments: %v", err)
-	}
-	for _, d := range sts.Items {
-		if d.GetLabels()["sync-to-ardoq"] != "disabled" {
-			b.OnApplicationResourceEvent(event, d)
-		}
-	}
-}
 func (b *BridgeController) OnNodeEvent(event watch.Event, res *v12.Node) {
 	resourceType := "Node"
 	if res.Name == "" {
--- main.go
+++ main.go
@@ -159,13 +159,11 @@ func main() {
 							subscriptions.DeploymentSubscriber{},
 							subscriptions.StatefulsetSubscriber{},
 							subscriptions.NodeSubscriber{},
-							subscriptions.NamespaceSubscriber{},
 						},
 					}, []watcher.IObject{
 						kubeClient.AppsV1().Deployments(""),
 						kubeClient.AppsV1().StatefulSets(""),
 						kubeClient.CoreV1().Nodes(),
-						kubeClient.CoreV1().Namespaces(),
 					})
 				if err != nil {
 					klog.Error(err)
