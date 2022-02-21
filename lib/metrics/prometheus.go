35b5981ed9a7aac61293ee3316e018e31606a5e6
Ian Muge <ian.muge@ardoq.com>
2022-02-21 17:15:21 +0100

    expose cache access metrics

--- lib/metrics/prometheus.go
+++ lib/metrics/prometheus.go
@@ -10,4 +10,16 @@ var (
 		Name: "k8sardoqbridge_event_ops_total",
 		Help: "The total number of processed events",
 	})
+	CacheHits = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "k8sardoqbridge_cache_hit_total",
+		Help: "Total Number of resources fetched from the cache",
+	})
+	CacheMiss = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "k8sardoqbridge_cache_miss_total",
+		Help: "Total Number of resources fetched from remote source",
+	})
+	CacheSets = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "k8sardoqbridge_cache_set_total",
+		Help: "Total Number of resources persisted to cache",
+	})
 )
