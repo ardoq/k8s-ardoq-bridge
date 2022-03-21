d0781b2a91c17ea86c675cf7ac3d6afffe8d5514
Ian Muge <ian.muge@ardoq.com>
2022-03-21 14:47:17 +0100

    we do not need to release from local

--- release_helm_chart.sh
+++ release_helm_chart.sh
@@ -7,14 +7,6 @@ if ! git diff --quiet; then
 fi
 
 HELM_DIR=helm/chart
-GH_OWNER=ardoq
-HELM_REP=k8s-ardoq-bridge
-
-if [[ -z ${CR_TOKEN} ]]
-then
-  echo "CR_TOKEN Not set in environment variables"
-  exit 1
-fi
 
 RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
 
@@ -81,51 +73,9 @@ sed -i "" "s/appVersion: .*/appVersion: $latest_tagged_version/g" $HELM_DIR/Char
 git add CHART_VERSION $HELM_DIR
 git commit -m "Upgraded helm chart version to $new_version appVersion to $latest_tagged_version"
 
-
 echo "Upgraded helm chart version to $new_version appVersion to $latest_tagged_version ready to be pushed"
 
 echo "Releasing Helm chart"
 
-function setup_chart_releaser() {
-  arch_name=$(uname -m)
-  kernel=$(uname | tr '[:upper:]' '[:lower:]')
-  case "$arch_name" in
-      amd64)  arch_name="amd64"                    ;;
-      x86_64) arch_name="amd64"                   ;;
-      arm64) arch_name="arm64"                  ;;
-  * ) echo    "Your Architecture '$arch_name' -> ITS NOT SUPPORTED."   ;;
-  esac
-  curl -OL https://github.com/helm/chart-releaser/releases/download/v1.3.0/chart-releaser_1.3.0_"${kernel}"_${arch_name}.tar.gz
-  tar xzvf chart-releaser_1.3.0_"${kernel}"_${arch_name}.tar.gz cr
-  chmod +x cr
-  rm chart-releaser_1.3.0_"${kernel}"_${arch_name}.tar.gz
-}
-function cleanup() {
-  rm cr
-}
-
-echo "Linting"
-helm lint $HELM_DIR || exit 1
-
-echo "setting up chart releaser"
-setup_chart_releaser || exit 1
-
-echo "package helm chart"
-./cr package $HELM_DIR -p helm || exit 1
-
-echo "Upload helm chart"
-./cr upload -o $GH_OWNER -r $HELM_REP --skip-existing -p helm || exit 1
-git add helm/k8s-ardoq-bridge-*
-git commit -m '[automated commit] uploaded archived helm chart'
-
-echo "Index Helm chart"
-git fetch origin gh-pages
-./cr index -o $GH_OWNER -r $HELM_REP -c https://raw.githubusercontent.com/$GH_OWNER/$HELM_REP/main/ -i helm/index.yaml -p helm --push || exit 1
-git add helm/index.yaml
-git commit -m '[automated commit] uploaded index file'
-
 echo "Push all staged changes"
 git push
-
-echo "cleanup"
-cleanup || exit 1
