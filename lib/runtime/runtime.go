921eadc8dede267b5431705e936d8a1bd62cbc2c
Ian Muge <ian.muge@ardoq.com>
2022-03-29 09:55:45 +0200

    exit when connection to kubernetes hangs up

--- lib/runtime/runtime.go
+++ lib/runtime/runtime.go
@@ -11,6 +11,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	k "k8s.io/client-go/kubernetes"
+	"os"
 	"sync"
 	"time"
 )
@@ -60,7 +61,6 @@ func EventBuffer(context context.Context, client k.Interface,
 					select {
 					case update, hasUpdate := <-c:
 						if hasUpdate {
-
 							err := registry.OnEvent(subscription.Message{
 								Event:  update,
 								Client: client,
@@ -70,10 +70,10 @@ func EventBuffer(context context.Context, client k.Interface,
 								return err
 							}
 							metrics.TotalEventOps.Inc()
-						}
-						if !hasUpdate {
+						} else {
 							// the channel got closed, so we need to restart
 							log.Error("Kubernetes hung up on us, restarting event watcher")
+							os.Exit(1)
 						}
 						//case <-time.After(30 * time.Minute):
 						//	// deal with the issue where we get no events
