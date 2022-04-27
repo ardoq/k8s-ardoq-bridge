595efa50948e663f7c5f4d0167b0fa4f2487ce11
Ian Muge <13362741+ianmuge@users.noreply.github.com>
2022-04-27 15:57:23 +0200

    WIP: try not to restart but rather restart the loop (#61)

--- lib/runtime/runtime.go
+++ lib/runtime/runtime.go
@@ -11,7 +11,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	k "k8s.io/client-go/kubernetes"
-	"os"
 	"sync"
 	"time"
 )
@@ -57,6 +56,7 @@ func EventBuffer(context context.Context, client k.Interface,
 			err := func(t int, c <-chan watch.Event) error {
 				defer wg.Done()
 				counter := 0
+			loop:
 				for {
 					select {
 					case update, hasUpdate := <-c:
@@ -73,7 +73,8 @@ func EventBuffer(context context.Context, client k.Interface,
 						} else {
 							// the channel got closed, so we need to restart
 							log.Error("Kubernetes hung up on us, exiting!")
-							os.Exit(1)
+							break loop
+							//os.Exit(1)
 						}
 						//case <-time.After(30 * time.Minute):
 						//	// deal with the issue where we get no events
@@ -81,6 +82,7 @@ func EventBuffer(context context.Context, client k.Interface,
 					}
 					counter++
 				}
+				return nil
 			}(x, o)
 			if err != nil {
 				log.Error(err)
